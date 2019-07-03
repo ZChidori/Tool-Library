ee0efefa0935698c2f9136ba9095340950476f8c
Chidori <hasfdah@users.noreply.github.com>
2019-07-03 11:20:45 +0800

    Update avg file size.cpp

--- avg file size.cpp
+++ avg file size.cpp
@@ -1,33 +1,58 @@
-//Calculate the average size of files in a folder.
-//Enter a folder with some subfolder,in which are whole normal files.
+//输入同目录下的另一个文件夹名，计算其下子文件夹内文件的平均文件大小。
+#define PATHBUF 100
 #include<iostream>
 #include<fstream>
 #include<unistd.h>
 #include<sys/stat.h>
 #include<dirent.h>
 #include<string.h>
+#include<vector>
+#include<algorithm>
 using namespace std;
+struct info
+{
+    info(int diravs,char dirname[])
+    {
+        avs=diravs;
+        strcpy(dir,dirname);
+    }
+    int avs;
+    char dir[PATHBUF];
+};
+bool sortfun(const info a,const info b)
+{
+    return a.avs>b.avs;
+}
 int main()
 {
-    char path[100],subdirpath[100],img[100];
+    char *path=getcwd(NULL,PATHBUF),dname[PATHBUF];
+    cin>>dname;
+    strcat(path,"/");
+    strcat(path,dname);
+    cout<<path<<endl;
+    vector<info>dirinfo;
     struct dirent *entry;
     struct stat statbuf;
     DIR *dir=opendir(path);
-    for(;(entry=readdir(dir))!=nullptr;)
+    for(;(entry=readdir(dir))!=NULL;)
     {
         stat(entry->d_name,&statbuf);
         if(strcmp(".",entry->d_name)==0||strcmp("..",entry->d_name)==0)
             continue;
+        char subdirpath[PATHBUF],subdirname[PATHBUF];
         strcpy(subdirpath,path);
         strcat(subdirpath,"/");
         strcat(subdirpath,entry->d_name);
-        float i=0,size=0;
+        strcpy(subdirname,entry->d_name);
+        int i=0;
+        float size=0;
         DIR *subdir=opendir(subdirpath);
-        for(;(entry=readdir(subdir))!=nullptr;)
+        for(;(entry=readdir(subdir))!=NULL;)
         {
             stat(entry->d_name,&statbuf);
             if(strcmp(".",entry->d_name)==0||strcmp("..",entry->d_name)==0)
                 continue;
+            char img[PATHBUF];
             strcpy(img,subdirpath);
             strcat(img,"/");
             strcat(img,entry->d_name);
@@ -35,7 +60,17 @@ int main()
             size+=statbuf.st_size;
             i++;
         }
-        cout<<subdirpath<<"average:"<<size/i<<"kB"<<endl;
+        int avs=size/i/1000;
+        if(avs>150)
+        {
+            info *targetdir=new info(avs,subdirname);
+            dirinfo.push_back(*targetdir);
+            delete targetdir;
+        }
     }
+    sort(dirinfo.begin(),dirinfo.end(),sortfun);
+    ofstream n;
+    n.open("info.txt",ios::trunc);
+    for(vector<info>::iterator t=dirinfo.begin();t!=dirinfo.end();n<<t->dir<<' '<<t++->avs<<'\n');
     return 0;
 }
