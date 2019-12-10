dbf7fc7734def78717a35c10bb65f8bad59a8502
ZChidori <16848861+ZChidori@users.noreply.github.com>
2019-12-10 14:41:43 +0800

    Update 律师函警告.py

--- 律师函警告.py
+++ 律师函警告.py
@@ -1,18 +1,9 @@
 import cv2
-cap=cv2.VideoCapture('footage.mp4')
 while True:
-    ret,frame=cap.read()
-    if frame is None:
-        break
-    img_gray=cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)#转灰度图
-    img_blurred=cv2.GaussianBlur(img_gray,(5,5),0)#高斯模糊
-    img_threshold1=cv2.adaptiveThreshold(img_blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,2)#二值化
-    img_threshold1_blurred=cv2.GaussianBlur(img_threshold1,(5,5),0)#二次模糊，消除噪点
-    _,img_threshold2=cv2.threshold(img_threshold1_blurred,200,255,cv2.THRESH_BINARY)#二次二值化，深化边缘
-    kernel=cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))#开运算，消除噪点
-    img_opening = cv2.bitwise_not(cv2.morphologyEx(cv2.bitwise_not(img_threshold2),cv2.MORPH_OPEN,kernel))
-    img_opening_blurred=cv2.GaussianBlur(img_opening,(3,3),0)#三次高斯模糊
-    cv2.imshow('img_opening_blurred',img_opening_blurred)
-    if cv2.waitKey(40)&0xFF==ord('q'):
-        break
+        frame=cv2.VideoCapture('footage.mp4').read()
+        if frame is None:
+                break
+        cv2.imshow('img_opening_blurred',cv2.GaussianBlur(cv2.bitwise_not(cv2.morphologyEx(cv2.bitwise_not(img_threshold2=cv2.threshold(cv2.GaussianBlur(cv2.adaptiveThreshold(cv2.GaussianBlur(cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY),(5,5),0),255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,5,2),(5,5),0),200,255,cv2.THRESH_BINARY)),cv2.MORPH_OPEN,cv2.getStructuringElement(cv2.MORPH_RECT,(3,3)))),(3,3),0))
+        if cv2.waitKey(40)&0xFF==ord('q'):
+                break
 cv2.destroyAllWindows()
